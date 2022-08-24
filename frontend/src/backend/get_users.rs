f3c13970d57853673c2cb012803b9fc27665f1d6
Aman Sharma <lunchspider03@gmail.com>
2022-08-24 18:47:09 +0530

    fixes github graphql request api

--- frontend/src/backend/get_users.rs
+++ frontend/src/backend/get_users.rs
@@ -1,23 +1,37 @@
-use serde::Deserialize;
-use reqwest::Error;
+use anyhow::Error;
+use graphql_client::{GraphQLQuery, Response};
+use reqwest::Client;
+use serde_derive::{Deserialize, Serialize};
 use web_sys::console::log_1;
-#[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32,
-}
 
-#[tokio::main]
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/backend/schema.graphql",
+    query_path = "src/backend/query.graphql",
+    response_derives = "Debug, Serialize, Deserialize"
+)]
+pub struct RepoView;
+
 async fn get_data() -> Result<(), Error> {
-    //TODO
-    // call a graphql api from hre
-    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-                              owner = "rust-lang-nursery",
-                              repo = "rust-cookbook");
-    println!("{}", request_url);
-    let response = reqwest::get(&request_url).await?;
-
-    let users: Vec<User> = response.json().await?;
-    log_1(&format!("{:#?}", users).into());
+    let github_token = std::env::var("GITHUB_API_TOKEN").expect("Github api token not found!");
+    let (owner, repo) = ("lunchspider", "quark");
+    let variables = repo_view::Variables {
+        owner: owner.into(),
+        name: repo.into(),
+    };
+    let request_body = RepoView::build_query(variables);
+    let client = Client::builder()
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str(&format!("Bearer {}", github_token))
+                    .unwrap(),
+            ))
+            .collect(),
+        )
+        .build()?;
+    let res = client.post("https://api.github.com/graphql").json(&request_body).send().await?;
+    let response_body: Response<repo_view::ResponseData> = res.json().await?;
+    log_1(&format!("{:?}", response_body).into());
     Ok(())
 }
