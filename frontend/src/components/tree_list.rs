81acd0d5da94778295308a4214ae3aa284367beb
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-09-04 13:07:09 +0300

    upgrade files tree

--- frontend/src/components/tree_list.rs
+++ frontend/src/components/tree_list.rs
@@ -33,9 +33,5 @@ pub fn tree_list(props: &TreeListProps) -> Html {
     //d.reduce_mut(|r| r.data.insert(225, "file four!".into()));
 
     //(props.files.clone()).into_iter().map(|file| file.to_html()).collect::<Html>()
-    html! {
-        <>
-        {  tree.adjacency.get(&0).unwrap().into_iter().map(|f| tree.to_html(*f)).collect::<Html>() }
-        </>
-    }
+    tree.adjacency.get(&0).unwrap().into_iter().map(|f| tree.to_html(*f)).collect::<Html>()
 }
--- frontend/src/utils/filetree.rs
+++ frontend/src/utils/filetree.rs
@@ -1,27 +1,71 @@
 use std::collections::{HashMap, HashSet};
-
+use wasm_bindgen::prelude::Closure;
 use serde::{Deserialize, Serialize};
 use yew::{html, Html};
-
-use web_sys::{Element, MouseEvent};
+use web_sys::console::log_1;
+use web_sys::{window, Element, MouseEvent, DragEvent};
 use yew::prelude::*;
 use yew_router::prelude::*;
+use wasm_bindgen::{UnwrapThrowExt, JsCast};
 
 use crate::router::Route;
 use yewdux::prelude::*;
 
+use crate::extensions::*;
 
 #[derive(PartialEq, Properties)]
 pub struct FileComponentProps {
     pub onclick: Callback<MouseEvent>,
-    pub children: Children,
+    pub name: String,
+    pub class: String,
 }
 
 
 #[function_component(FileComponent)]
 pub fn file_component(props: &FileComponentProps) -> Html {
+    // let classname = use_state_eq(|| "dropdown-content".to_string());
+
+    let handle_dragenter: Callback<DragEvent> = Callback::from(move |e: DragEvent| {
+        // let target = e.terget().unwrap();
+        // TODO
+        //  target.set_attribute("style", "background:lightblue");
+    });
+
+    let handle_dragleave: Callback<DragEvent> = Callback::from(move |e: DragEvent| {
+        // let target = e.target().unwrap();
+        // TODO
+        //  target.set_attribute("style", "background:none");
+    });
+
+    //
+    // let f = classname.clone();
+    // let handle_mousedown: Callback<MouseEvent> = Callback::from(move |e: MouseEvent| {
+    //     &e.prevent_default();
+    //     let doc = window().unwrap_throw().document().unwrap_throw();
+    //     if e.which() == 3 {
+    //         f.set("show dropdown-content".to_string())
+    //     } else {
+    //         f.set("show dropdown-content".to_string())
+    //     }
+    // });
+
+    // log_1(&format!("{:?}", *classname).into());
+
     html! {
-        <li onclick = { props.onclick.clone() } style="flex: 1 1 auto; white-space: nowrap; min-width: 0px; overflow: hidden; text-overflow: ellipsis; display: flex; align-items: center;"><div class="notranslate" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{props.children.clone()}</div></li>
+    <>
+        <li
+         // onmousedown={handle_mousedown}
+         ondragleave={handle_dragleave}
+         ondragenter={handle_dragenter}
+         draggable="true"
+         class={props.class.clone()}
+         onclick = { props.onclick.clone() } style="flex: 1 1 auto; white-space: nowrap; min-width: 0px; overflow: hidden; text-overflow: ellipsis; display: flex; align-items: center;"><div class="notranslate" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{props.name.clone()}</div></li>
+        <div class={"dropdown-content"}>
+        <a href="#">{"☁"}</a>
+        <a href="#">{"🗑"}</a>
+        <a href="#">{"👁"}</a>
+        </div>
+    </>
     }
 }
 
@@ -100,12 +144,16 @@ impl FileTree {
             }
         });
 
+
         // avoid repetition
         // i found `<li class={class_name}  onclick = { handle_click }>{&self.vertices.get(&start).unwrap().name}</li>` was repeated three times
         // now i have `let Some(nodes) = nodes` repeated two times but I don't know how to avoid that.
+
+
         return html! {
         <>
-            <li class={class_name}  onclick = { handle_click }>{&self.vertices.get(&start).unwrap().name}</li>
+            <FileComponent class={format!("file_component hovering {}",class_name)} onclick={handle_click} name={self.vertices.get(&start).unwrap().name.clone()}/>
+
             if let Some(nodes) = nodes {
                 { if has_children{
 
@@ -114,8 +162,6 @@ impl FileTree {
                 </ul>}
             } else{ html!{"+ Create new file."}}}
             }
-
-
         </>
         };
     }
