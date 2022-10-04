3c3c91f9177dc072fd302d2c0db9629e9352188a
Ali Sci <58806996+aliscie@users.noreply.github.com>
2022-10-04 17:09:28 +0300

    fix

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,6 +1,7 @@
 extern crate futures;
 
 use std::env;
+use wasm_bindgen_futures::spawn_local;
 
 use app::App;
 use dotenv::dotenv;
@@ -20,14 +21,6 @@ mod router;
 mod app;
 // 0.3.1
 
-async fn example() {
-    // dotenv().ok();
-    // let BACKEND_CANISTER: &str = &std::env::var("BACKEND_CANISTER").expect("BACKEND_CANISTER is not set");
-    let BACKEND_CANISTER = "ug5r5-74qxz-4dkqw-bjx6f-2wlit-3pqay-unom3-memsb-xgv4t-ljf2z-sae";  // The management canister ID.
-    let principal = Principal::from_text(BACKEND_CANISTER).expect("Could not decode the principal.");
-    // let greeting: = ic_cdk::call(principal, "greet", ("Ali", "" )).await;
-    // web_sys::console::log_1(&format!("onpaste {:?}", greeting.unwrap()).into());
-}
 
 
 lazy_static! {
@@ -45,7 +38,14 @@ lazy_static! {
 
 
 fn main() {
-    let v = executor::block_on(example());
+    spawn_local(async move {
+        // dotenv().ok();
+        // let BACKEND_CANISTER: &str = &std::env::var("BACKEND_CANISTER").expect("BACKEND_CANISTER is not set");
+        let BACKEND_CANISTER = "ug5r5-74qxz-4dkqw-bjx6f-2wlit-3pqay-unom3-memsb-xgv4t-ljf2z-sae";  // The management canister ID.
+        let principal = Principal::from_text(BACKEND_CANISTER).expect("Could not decode the principal.");
+        // let greeting: = ic_cdk::call(principal, "greet", ("Ali", "" )).await;
+        // web_sys::console::log_1(&format!("onpaste {:?}", greeting.unwrap()).into());
+    });
 
 
     yew::start_app::<App>();
