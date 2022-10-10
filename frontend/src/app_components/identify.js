0b94015622eaaa3b0a89d6e62bc8284c14b57ed8
Aman Sharma <lunchspider03@gmail.com>
2022-10-10 23:55:39 +0530

    this fixes dfinity identity error

--- frontend/src/app_components/identify.js
+++ frontend/src/app_components/identify.js
@@ -1,19 +1,19 @@
 // TODO
 //  TypeError: Module specifier does not start with "/", "./", or "../".
 //  promiseReactionJob
-//  import {AuthClient} from '@dfinity/auth-client';
+import { AuthClient } from "@dfinity/auth-client";
 
 export async function identify() {
-    // const authClient = await AuthClient.create();
-    // if (authClient.isAuthenticated()) {
-    //     return "already logged in";
-    // } else {
-    //     authClient.login({
-    //         identityProvider: 'https://identity.ic0.app/#authorize',
-    //         onSuccess: () => {
-    //             return "login success";
-    //         }
-    //     })
-    // }
-    return "already logged in 2";
+    const authClient = await AuthClient.create();
+    console.log(authClient);
+    if (authClient.isAuthenticated()) {
+        return "already logged in";
+    } else {
+        authClient.login({
+            identityProvider: "https://identity.ic0.app/#authorize",
+            onSuccess: () => {
+                return "login success";
+            },
+        });
+    }
 }
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -2,12 +2,9 @@ extern crate futures;
 extern crate wasm_bindgen_futures;
 extern crate yew;
 extern crate shared;
-
 use app::App;
-use ic_cdk;
 use lazy_static::lazy_static;
 use wasm_bindgen::prelude::*;
-
 mod backend;
 mod components;
 mod app_components;
@@ -21,6 +18,7 @@ use web_sys;
 
 lazy_static! {
     pub static ref IS_WEB: bool = {
+        #[allow(unused_mut)]
         let mut m : bool = false;
         #[cfg(feature = "web")] {
             m = true;
@@ -47,4 +45,3 @@ pub fn run() {
     // });
     yew::start_app::<App>();
 }
-
