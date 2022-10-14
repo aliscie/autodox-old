f6152b030baa015eac3a61033835aeecb8c2d822
aliscie <58806996+aliscie@users.noreply.github.com>
2022-10-14 11:35:30 +0300

    fixes

--- backend/storage/tests/test_storage.rs
+++ backend/storage/tests/test_storage.rs
@@ -1,8 +1,17 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn check_if_everything_works_ok() {
-    let storage = InfoStorage::new();
-    ...
-  }
-}
+// #[init]
+// fn init() {
+//   unsafe {
+//     STATE = Some(InfoStorage::new())
+//   }
+// }
+//
+// #[update]
+// fn set_info(info: String) {
+//   let state = unsafe { STATE.as_mut().unwrap() };
+//   state.set_info(info);
+// }
+//
+// #[query]
+// fn get_info() -> String {
+//   unsafe { STATE.as_ref().unwrap().get_info() }
+// }
