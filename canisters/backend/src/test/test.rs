d08c2f6bedce8d030e01b228f7afd533d2efc373
aliscie <58806996+aliscie@users.noreply.github.com>
2022-11-22 11:18:16 +0300

    tests

---
+++ canisters/backend/src/test/test.rs
@@ -0,0 +1,26 @@
+use crate::*;
+
+use ic_kit::{
+    macros::*,
+    candid::export_service
+};
+
+#[query(name = "__get_candid_interface_tmp_hack")]
+fn export_candid() -> String {
+    export_service!();
+    __export_service()
+}
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    pub fn save_candid() {
+        use std::env;
+        use std::fs::write;
+        use std::path::PathBuf;
+
+        let dir = PathBuf::from(env::current_dir().unwrap());
+        write(dir.join("backend.did"), export_candid()).expect("Write failed.");
+    }
+}
