d1b4e70b50b865add0d0e17adacb9d429809437a
lunchspider <lunchspider03@gmail.com>
2022-11-23 18:00:50 +0530

    bug fixes

---
+++ tauri-src/src/context.rs
@@ -0,0 +1,20 @@
+use std::sync::Arc;
+
+use crate::store::Store;
+
+pub struct Context {
+    pub store: Arc<Store>,
+}
+
+impl Context {
+    #[inline]
+    pub fn new(store: Store) -> Self {
+        Self {
+            store: Arc::new(store),
+        }
+    }
+    #[inline]
+    pub fn get_store(&self) -> Arc<Store> {
+        self.store.clone()
+    }
+}
