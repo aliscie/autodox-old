caeca9ee795428f91cbfa418782736c12bc6edba
aliscie <58806996+aliscie@users.noreply.github.com>
2022-11-27 14:47:33 +0300

    cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,5 @@
 import {defineConfig} from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
     build: {
