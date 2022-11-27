216026cd304323ab17d91c87319e5a4e90c7bab9
aliscie <58806996+aliscie@users.noreply.github.com>
2022-11-27 14:39:17 +0300

    fix vite

--- frontend/index.js
+++ frontend/index.js
@@ -1,25 +1,8 @@
 if (typeof BigInt === 'undefined') global.BigInt = require('big-integer')
-// the above line to solve the error
-//     [Error] Unhandled Promise Rejection: ReferenceError: Can't find variable: BigInt
-//         (anonymous function)
-//         promiseReactionJob
 
-// webpack config
-//import('./pkg').then((module) => {
-    //module.run()
-//})
+import init, {run} from './pkg/frontend.js';
 
-// vite config
-import init , {run} from 'frontend';
 init().then(() => {
-    consolg.log('init wasm-pack vite');
     run()
 })
 
-// import { defineConfig } from 'vite';
-// import wasmPack from 'vite-plugin-wasm-pack';
-//
-// export default defineConfig({
-//   // pass your local crate path to the plugin
-//   plugins: [wasmPack('./pkg')]
-// });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-    build:{
-        minify : false
+    build: {
+        minify: false
     },
-    // pass your local crate path to the plugin
-    plugins: [wasmPack(["../frontend"])]
+    plugins: [
+        wasmPack(["../frontend"])
+    ]
 });
