aeb76982f084b77b574f3ca845880f511e452989
lunchspider <lunchspider03@gmail.com>
2022-12-15 15:04:10 +0530

    fixes file delete call

--- frontend/src/specific_components/file_component.rs
+++ frontend/src/specific_components/file_component.rs
@@ -2,6 +2,7 @@ use uuid::Uuid;
 use web_sys::{Element, MouseEvent};
 use yew::prelude::*;
 use yew_hooks::use_toggle;
+use yew_router::prelude::{use_navigator, use_route};
 use yewdux::prelude::*;
 
 use shared::{
@@ -10,7 +11,7 @@ use shared::{
     schema::{FileDirectory, FileNodeDelete},
 };
 
-use crate::components::Menu;
+use crate::{components::Menu, router::Route};
 
 #[derive(PartialEq, Properties)]
 pub struct FileComponentProps {
@@ -36,6 +37,8 @@ pub fn file_component(props: &FileComponentProps) -> Html {
     let is_dragged = use_state(|| "".to_string());
     let is_enter = use_state(|| "".to_string());
     let position: UseStateHandle<Option<MouseEvent>> = use_state(|| None);
+    let navigator = use_navigator().unwrap();
+    let route = use_route::<Route>().unwrap_or_default();
 
     let caret = use_toggle("", "caret-down");
     let id = props.id.clone();
@@ -150,12 +153,21 @@ pub fn file_component(props: &FileComponentProps) -> Html {
             parent_id,
             tree_id: file_dispatch.get().id,
         };
+        let file_id = id.clone();
         file_dispatch.reduce_mut_future_callback(move |state| {
+            match route {
+                // the current file is in use navigate to home!
+                Route::File { id } => {
+                    if file_id == id {
+                        navigator.push(&Route::Home);
+                    }
+                }
+                _ => {}
+            }
             Box::pin(async move {
-                let id = id.clone();
                 let result = crate::backend::delete_file(delete_file_node).await;
                 log!(result);
-                state.files.remove(&id);
+                state.files.remove(&file_id);
             })
         })
     };
@@ -230,7 +242,7 @@ pub fn file_component(props: &FileComponentProps) -> Html {
            html! {<a><i class="fa-solid fa-signature"></i>{"Rename"}</a>},
            html! {<a><i class="fa-solid fa-upload"/>{"Share"}</a>},
            html! {<a><i class="fa-solid fa-eye"/>{"Permissions"}</a>},
-           html! {<a><i class="fa-solid fa-trash" onclick = {ondelete}/>{"Delete"}</a>},
+           html! {<a onclick = {ondelete}><i class="fa-solid fa-trash"/>{"Delete"}</a>},
            html! {<a><i class="fa-brands fa-medium"></i>{"Category"}</a>},
            ]}
            event={position.clone()}
