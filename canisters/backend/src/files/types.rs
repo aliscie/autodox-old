baaacc67459f6e47f539df8ec5236fe51f7a43bc
aliscie <alihushamsci@icloud.com>
2022-12-21 19:02:16 +0300

    refactor

--- canisters/backend/src/files/types.rs
+++ canisters/backend/src/files/types.rs
@@ -2,32 +2,22 @@ use ic_stable_memory::collections::vec::SVec;
 use std::collections::HashMap;
 pub type MyStrings = SVec<String>;
 pub type MyStringsSlice = Vec<String>;
-use serde::{Deserialize, Serialize};
 use candid::CandidType;
+use serde::{Deserialize, Serialize};
 
-use shared::{schema::{FileMode, FileDirectory, FileNode, ElementTree}, id::Id, Tree};
+use shared::{
+    id::Id,
+    schema::{ElementTree, FileDirectory, FileMode, FileNode},
+    Tree,
+};
 pub type Files = HashMap<Id, Tree<Id, FileNode>>;
 
 #[derive(Debug, Serialize)]
 #[cfg_attr(feature = "backend", derive(Readable, Writable))]
-
 #[derive(CandidType, Deserialize)]
 pub struct CreateFileData {
     pub parent_id: String,
     pub name: String,
     pub mode: FileMode,
     pub content: String,
-}
-
-
-
-pub enum CreateResult{
-    Ok,
-    AlreadyExist,
-    DirectoryDoesNotExist
-}
-
-#[derive(Serialize, Debug)]
-pub struct RegisterResponse {
-    url: String,
 }
--- canisters/backend/src/files/update.rs
+++ canisters/backend/src/files/update.rs
@@ -1,56 +1,56 @@
-use ic_kit::candid::candid_method;
-use ic_cdk::update;
-use ic_stable_memory::s;
+use ic_kit::{candid::candid_method, macros::*};
+use std::collections::HashMap;
+
+use crate::files::types::*;
+use crate::users::types::*;
+use crate::utils::{get_username, Status, UpdateResponse};
+use candid::CandidType;
+use ic_stable_memory::collections::vec::SVec;
 use ic_stable_memory::utils::ic_types::SPrincipal;
-use crate::files::types::RegisterResponse;
-use crate::users;
-use crate::users::types::{User, Users};
-use crate::utils::Status;
+use ic_stable_memory::{
+    s, stable_memory_init, stable_memory_post_upgrade, stable_memory_pre_upgrade,
+};
+use shared::id::Id;
+use shared::schema::{FileDirectory, FileNode, FileNodeCreate};
 
-// use crate::update;
-// use ic_stable_memory::{
-//     s, utils::ic_types::SPrincipal,
-// };
-//
-// use shared::{
-//     schema::*, id::Id,
-// };
-//
-// // use crate::*;
-// use crate::files::types::RegisterResponse;
+#[update]
+#[candid_method(update)]
+pub fn create_file(create_file_data: FileNodeCreate) -> Status {
+    let caller = SPrincipal(ic_cdk::caller());
+    let users = s!(Users);
+    let username = match get_username(caller, &users) {
+        None => return Status::UnAuthorized,
+        Some(username) => username,
+    };
+    let mut user_files: UserFiles = s!(UserFiles);
+    if let Some(file_directory) = user_files.get_mut(username) {
+        let mut parent_adjacency = file_directory
+            .adjacency
+            .entry(create_file_data.parent_id)
+            .or_default();
+        parent_adjacency.insert(create_file_data.id);
+        file_directory
+            .vertices
+            .insert(create_file_data.id, create_file_data.into());
+    }
+    // let _= create::utils::_create_file(&mut user_files, &username, create_file_data.directory_id, create_file_data.id, create_file_data.name, create_file_data.children);
+    s! { UserFiles = user_files};
+    Status::Success
+}
 
-// #[update]
-// #[candid_method(update)]
-// pub fn create_directory(directory_id: Id, name: String) -> CreateDirectoryResponse{
-//     let caller = SPrincipal(ic_cdk::caller());
-//     let users = s!(Users);
-//     let username = match get_username(caller, &users){
-//         None => return CreateDirectoryResponse::UserNotRegisted,
-//         Some(username) => username
-//     };
-//     let mut user_files = s!(UserFiles);
-//     match _create_directory(&mut user_files, &username, directory_id, name){
-//         CreateResult::AlreadyExist => return CreateDirectoryResponse::AlreadyExist,
-//         _ => ()
-//     }
-//     s!{ UserFiles = user_files};
-//     CreateDirectoryResponse::Success
-// }
-
-// #[update]
-// #[candid_method(update)]
-// pub fn create_file(fnc: FileNodeCreate) -> Result<(), BackendError>{
-//     let caller = SPrincipal(ic_cdk::caller());
-//     let users = s!(Users);
-//     let username = match get_username(caller, &users){
-//         None => return Err(BackendError::UserNotRegisted),
-//         Some(username) => username
-//     };
-//     let mut user_files = s!(UserFiles);
-//     match _create_file(&mut user_files, &username, fnc.directory_id, fnc.id, fnc.name, fnc.children){
-//         CreateResult::Ok => (),
-//         _ => return Err(BackendError::DirectoryDoesNotExist)
-//     }
-//     s!{ UserFiles = user_files};
-//     Ok(())
-// }
+#[update]
+#[candid_method(update)]
+pub fn create_directory(create_file_data: FileDirectory) -> Status {
+    let caller = SPrincipal(ic_cdk::caller());
+    let users = s!(Users);
+    let username = match get_username(caller, &users) {
+        None => return Status::UnAuthorized,
+        Some(username) => username,
+    };
+    let mut user_files = s!(UserFiles);
+    user_files.insert(username, create_file_data);
+    // let res = _create_directory(&mut user_files, &username, create_file_data.id, create_file_data.name);
+    // println!("{:#?}", res);
+    s! { UserFiles = user_files};
+    Status::Success
+}
