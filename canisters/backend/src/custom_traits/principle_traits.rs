2a5b725ecf6e71d5685cf24c382713a91b8e8a74
aliscie <alihushamsci@icloud.com>
2022-12-24 20:36:36 +0300

    add principle generator trait

--- canisters/backend/src/custom_traits/principle_traits.rs
+++ canisters/backend/src/custom_traits/principle_traits.rs
@@ -2,6 +2,8 @@ use ic_cdk::export::Principal;
 
 pub trait PrincipalCustomTraits {
     fn is_anonymous() -> bool;
+
+    // async fn rand_row() -> [u8; 16];
 }
 
 impl PrincipalCustomTraits for Principal {
@@ -9,4 +11,13 @@ impl PrincipalCustomTraits for Principal {
         let _caller: Principal = ic_cdk::caller();
         return _caller.to_text() == "2vxsx-fae".to_string();
     }
+    // async fn rand_row() -> [u8; 16] {
+    //     ic_cdk::api::call::call(
+    //         ic_cdk::export::Principal::management_canister(),
+    //         "raw_rand",
+    //         (),
+    //     )
+    //         .await
+    //         .unwrap()
+    // }
 }
