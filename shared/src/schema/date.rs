f97146a60f420bef7056f28c2191619ecefd3d59
aliscie <alihushamsci@icloud.com>
2022-12-26 14:57:42 +0300

    fix names

--- shared/src/schema/date.rs
+++ shared/src/schema/date.rs
@@ -1,24 +1,28 @@
 use  chrono::DateTime;// this might be useful
 const current_timezone: String = web_sys::Storage::local_storage().unwrap().get_item("timezone").unwrap().unwrap();
 
-struct Date {
+struct DateTimeFiled {
+    string,
+}
+
+struct TimeField {
     string,
 }
 
 
-impl Date {
+impl DateTimeFiled {
     fn new(string: String) -> Self {
         Self { string }
     }
 }
 
-impl From<String> for Date {
+impl From<String> for DateTimeFiled {
     fn from(date: string) -> Self {
         date.convet_to_utc_timzone(from:current_timezone)
     }
 }
 
-impl To<String> for Date {
+impl To<String> for DateTimeFiled {
     fn to(date: string) -> Self {
         date.convet_current_user_timezone(to:current_timezone)
     }
