e38ab5600e74150b1522d4d9d3a0b08f5d8ae40c
aliscie <alihushamsci@icloud.com>
2022-12-27 19:03:31 +0300

    fix encode_image

--- frontend/src/specific_components/title_avatar_component.rs
+++ frontend/src/specific_components/title_avatar_component.rs
@@ -1,10 +1,10 @@
 use wasm_bindgen_futures::spawn_local;
-use web_sys::{ HtmlInputElement};
+use web_sys::{HtmlInputElement};
 use yew::prelude::*;
 use crate::backend;
 use crate::components::{Avatar, PopOverMenu};
 use shared::*;
-use js_sys::{ Uint8Array};
+use js_sys::{Uint8Array};
 use wasm_bindgen::JsValue;
 use web_sys::Url;
 use crate::utils::{decode_image, encode_image};
@@ -14,18 +14,21 @@ pub fn title_avatar_component() -> Html {
     let image_link: UseStateHandle<Option<String>> = use_state(|| None);
     let position: UseStateHandle<Option<MouseEvent>> = use_state(|| None);
     let _position = position.clone();
-
-
     let open_popover: Callback<MouseEvent> = Callback::from(move |_e: MouseEvent| {
         _position.set(Some(_e));
     });
 
     let _image_link = image_link.clone();
-    spawn_local(async move {
-        // let response =  backend::get_profile().await;
-        // decode_image(response);
-        _image_link.set(Some("https://avatars.githubusercontent.com/u/58806996?v=4".to_string()));
-    });
+    let image: Vec<u8> = vec![137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 10, 0, 0, 0, 10, 8, 6, 0, 0, 0, 141, 50, 207, 189, 0, 0, 0, 1, 115, 82, 71, 66, 0, 174, 206, 28, 233, 0, 0, 0, 68, 101, 88, 73, 102, 77, 77, 0, 42, 0, 0, 0, 8, 0, 1, 135, 105, 0, 4, 0, 0, 0, 1, 0, 0, 0, 26, 0, 0, 0, 0, 0, 3, 160, 1, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 160, 2, 0, 4, 0, 0, 0, 1, 0, 0, 0, 10, 160, 3, 0, 4, 0, 0, 0, 1, 0, 0, 0, 10, 0, 0, 0, 0, 59, 120, 184, 245, 0, 0, 0, 113, 73, 68, 65, 84, 24, 25, 133, 143, 203, 13, 128, 48, 12, 67, 147, 94, 97, 30, 24, 0, 198, 134, 1, 96, 30, 56, 151, 56, 212, 85, 68, 17, 88, 106, 243, 241, 235, 39, 42, 183, 114, 137, 12, 106, 73, 236, 105, 98, 227, 152, 6, 193, 42, 114, 40, 214, 126, 50, 52, 8, 74, 183, 108, 158, 159, 243, 40, 253, 186, 75, 122, 131, 64, 0, 160, 192, 168, 109, 241, 47, 244, 154, 152, 112, 237, 159, 252, 105, 64, 95, 48, 61, 12, 3, 61, 167, 244, 38, 33, 43, 148, 96, 3, 71, 8, 102, 4, 43, 140, 164, 168, 250, 23, 219, 242, 38, 84, 91, 18, 112, 63, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130];
+    let x = decode_image(image);
+    // log!(x);
+
+    // spawn_local(async move {
+    //     // let response =  backend::get_profile().await;
+
+    //     _image_link.set(Some("https://avatars.githubusercontent.com/u/58806996?v=4".to_string()));
+    // });
+
     let logout = Callback::from(move |e: MouseEvent| {
         spawn_local(async move {
             backend::logout().await;
@@ -35,9 +38,10 @@ pub fn title_avatar_component() -> Html {
     let on_upload: Callback<Event> = Callback::from(move |_e: Event| {
         let input: HtmlInputElement = _e.target_unchecked_into();
 
-        let buffer_bytes = encode_image(input.files().unwrap().get(0).unwrap());
+
         spawn_local(async move {
-            // log!(buffer_bytes);
+            let buffer_bytes = encode_image(input.files().unwrap().get(0).unwrap()).await;
+            log!(buffer_bytes);
             // let response =  backend::update_profile( Some(buffer_bytes), None).await;
         });
     });
@@ -64,17 +68,16 @@ pub fn title_avatar_component() -> Html {
             // log!(user_token);
         });
     });
-
-    let is_logged_ing = use_state(|| false);
+    let auth = true; // serde_wasm_bindgen::from_value::<bool>(backend::is_logged().await).unwrap();
+    let is_logged_ing = use_state(|| true);
     let _is_logged_ing = is_logged_ing.clone();
-    spawn_local(async move {
-        let auth = serde_wasm_bindgen::from_value::<bool>(backend::is_logged().await).unwrap();
-        _is_logged_ing.set(auth);
-    });
+
     if *is_logged_ing {
         return html! { <>
+
         <PopOverMenu {items} position = {position.clone()}/>
         <span class="right_clickable main_avatar" onclick={open_popover}>
+        <img src={x}/>
         <Avatar
             // src={image_link.clone()} TODO fix this
             />
--- frontend/src/utils/decode_image.rs
+++ frontend/src/utils/decode_image.rs
@@ -1,11 +1,13 @@
-use js_sys::{Promise,Uint8Array};
-use web_sys::File;
+use js_sys::{Promise, Uint8Array};
+use web_sys::{BlobPropertyBag, File, Url};
 use web_sys::{Blob};
+
 pub fn decode_image(image: Vec<u8>) -> Option<String> {
-    // TODO
-    //  let image_url  = Uint8Array::new(&image);
-    //  let x = Url.create_object_url_with_u8_array(Uint8Array::new(&bytes)).unwrap();
-    //  let bytes: Vec<u8> = Uint8Array::new(&buffer).try_into().unwrap();
-    //  let image_url = Blob::new_with_buffer_source_sequence_and_options(&vec![bytes], "image/png").unwrap();
-    Some("".to_string())
+    let image = Uint8Array::from(image.as_slice());
+    let mut optoins = BlobPropertyBag::new();
+    let mut optoins = optoins.type_("image/png");
+    let blob = Blob::new_with_u8_array_sequence_and_options(&image, &optoins).unwrap();
+    let url: String = Url::create_object_url_with_blob(&blob).unwrap();
+    // let image_url = Blob::new_with_buffer_source_sequence_and_options(&vec![bytes], "image/png").unwrap();
+    Some(url)
 }
