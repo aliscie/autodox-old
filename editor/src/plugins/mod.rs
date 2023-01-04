ffef0d27dc3f1830d868eef9aa9c2cb7bbb32a39
aliscie <alihushamsci@icloud.com>
2023-01-04 09:36:05 +0300

    make insert

--- editor/src/plugins/mod.rs
+++ editor/src/plugins/mod.rs
@@ -1,13 +1,15 @@
+use web_sys::Element;
+
+pub use editor_toolbar_plugin::*;
+pub use inset_component::*;
+pub use paste_converter::PasteConverter;
+
 mod paste_converter;
 mod inset_component;
+
 mod drag_and_drop;
 mod mention;
 mod editor_toolbar_plugin;
-pub use editor_toolbar_plugin::*;
-
-pub use paste_converter::PasteConverter;
-
-use web_sys::Element;
 
 // use web_sys::{Element, MouseEvent, window, Document};
 // use wasm_bindgen::UnwrapThrowExt;
@@ -20,8 +22,8 @@ pub trait EditorPlugin {
 pub trait PluginTraits {}
 
 impl<T> EditorPlugin for T
-where
-    T: PluginTraits,
+    where
+        T: PluginTraits,
 {
     // fn remove_menu<'a>(curr: &'a Element, doc: &'a Document) {}
     // fn make_menu<'a>( curr: &'a Element, doc: &'a Document) {
