eb4c4012639bbb893952679ecf1aa90bd1bf26c6
UltraBot <80860637+ultrabot2022@users.noreply.github.com>
2023-01-10 11:21:56 -0800

    show wallet once

--- frontend/src/backend/ic_agent.js
+++ frontend/src/backend/ic_agent.js
@@ -63,14 +63,18 @@ export const get_actor = async () => {
 			let publicKey
 
 			try {
-				publicKey = await plug.requestConnect({
-					whitelist: [process.env.BACKEND_CANISTER_ID],
-					host: process.env.DFX_NETWORK === "ic" ? 'https://mainnet.dfinity.network' : 'http://localhost:8510',
-					timeout: 50000,
-					onConnectionUpdate: () => {
-						console.log('sessionData: ', plug.sessionManager.sessionData)
-					},
-				});
+				const isConnected = await plug.isConnected();
+
+				if (!isConnected) {
+					publicKey = await plug.requestConnect({
+						whitelist: [process.env.BACKEND_CANISTER_ID],
+						host: process.env.DFX_NETWORK === "ic" ? 'https://mainnet.dfinity.network' : 'http://localhost:8510',
+						timeout: 50000,
+						onConnectionUpdate: () => {
+							console.log('sessionData: ', plug.sessionManager.sessionData)
+						},
+					});
+				}
 			} catch (e) {
 				console.error(e)
 				return
