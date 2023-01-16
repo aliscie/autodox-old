49c035736eaffb920fbdd4c33365b1f4bb344de9
lunchspider <lunchspider03@gmail.com>
2023-01-16 20:59:06 +0530

    switches to Option<FileDirectory> in tauri command and frontend

--- frontend/src/hooks/initialize_files.rs
+++ frontend/src/hooks/initialize_files.rs
@@ -1,18 +1,21 @@
-use yewdux::prelude::Dispatch;
 use shared::schema::FileDirectory;
+use yewdux::prelude::Dispatch;
 
 pub async fn init_files() -> Result<(), String> {
-    let mut directories = crate::backend::get_directories().await.map_err(|e| {
-        e
-    }).unwrap_or_default();
+    let mut directories = crate::backend::get_directories()
+        .await
+        .map_err(|e| e)
+        .unwrap_or_default();
     let file_tree = Dispatch::<FileDirectory>::new();
-    if directories.len() == 0 {
-        let file_directory = FileDirectory::default();
-        let x = crate::backend::create_directory(&file_directory).await;
-        file_tree.set(file_directory);
-    } else {
-        let x = directories.remove(0);
-        file_tree.set(x);
+    match directories {
+        Some(x) => {
+            file_tree.set(x);
+        }
+        None => {
+            let file_directory = FileDirectory::default();
+            let x = crate::backend::create_directory(&file_directory).await;
+            file_tree.set(file_directory);
+        }
     }
     Ok(())
 }
