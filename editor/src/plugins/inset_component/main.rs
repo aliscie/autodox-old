e740ade789646ae143787a64d283e5a3100be862
aliscie <alihushamsci@icloud.com>
2023-01-16 16:37:43 +0300

    remove /command after executing

--- editor/src/plugins/inset_component/main.rs
+++ editor/src/plugins/inset_component/main.rs
@@ -1,22 +1,37 @@
-use markup5ever_rcdom::Node;
+use std::collections::btree_map::Range;
 use wasm_bindgen::closure::Closure;
-use wasm_bindgen::JsCast;
-use web_sys::{Element, KeyboardEvent};
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
+use web_sys::{Element, KeyboardEvent, Node, window};
 use shared::log;
 
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "/src/plugins/inset_component/caret_position.js")]
+extern "C" {
+    #[wasm_bindgen(js_name = getCaretPosition)]
+    pub fn get_caret_position() -> u32;
+}
+
+
 pub fn insert_components(editor: &Element, trigger: String) {
+    let doc = window().unwrap_throw().document().unwrap_throw();
     let mut text_value: String = "".to_string();
     let mut track: bool = false;
+    let _editor = &doc.query_selector("#text_editor").unwrap().unwrap().query_selector("p").unwrap().unwrap();
+    let _editor = _editor.clone();
+    let mut range = window().unwrap_throw().document().unwrap_throw().create_range().unwrap();
+    let mut i = 0;
+    let mut f = 0;
 
     let handle_keydown = Closure::wrap(Box::new(move |e: KeyboardEvent| {
-        // let node: &Node = editor.into();
-        // let mut range = None;
-
-
         if track && &e.key() == "Enter" || e.key() == "Tab" {
-            // range.move_to_current();
-            // range.select();
-            // range.delete();
+            f = get_caret_position();
+
+            range.set_start(_editor.first_child().unwrap().as_ref(), i);
+            range.set_end(_editor.first_child().unwrap().as_ref(), f);
+            range.delete_contents();
+            i = 0;
+            f = 0;
         }
         if &e.key() == "Tab" {
             track = false;
@@ -34,7 +49,7 @@ pub fn insert_components(editor: &Element, trigger: String) {
 
         if e.key() == trigger {
             track = true;
-            // range = Some(node.create_text_range())
+            i = get_caret_position();
         }
     }) as Box<dyn FnMut(_)>);
 
