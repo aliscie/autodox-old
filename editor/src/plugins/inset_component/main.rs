aadf224225331d901aee5b78928f08869de6626e
aliscie <alihushamsci@icloud.com>
2023-01-17 11:51:17 +0300

    Dynamically get current focused element

--- editor/src/plugins/inset_component/main.rs
+++ editor/src/plugins/inset_component/main.rs
@@ -1,10 +1,9 @@
-use std::collections::btree_map::Range;
-use wasm_bindgen::closure::Closure;
 use wasm_bindgen::{JsCast, UnwrapThrowExt};
-use web_sys::{Element, KeyboardEvent, Node, window};
-use shared::log;
-
+use wasm_bindgen::closure::Closure;
 use wasm_bindgen::prelude::*;
+use web_sys::{Element, KeyboardEvent, Node, window};
+
+// use shared::log;
 
 #[wasm_bindgen(module = "/src/plugins/inset_component/caret_position.js")]
 extern "C" {
@@ -14,28 +13,25 @@ extern "C" {
 
 
 pub fn insert_components(editor: &Element, trigger: String) {
-    let doc = window().unwrap_throw().document().unwrap_throw();
     let mut text_value: String = "".to_string();
     let mut track: bool = false;
-    let _editor = &doc.query_selector("#text_editor").unwrap().unwrap().query_selector("p").unwrap().unwrap();
-    let _editor = _editor.clone();
     let mut range = window().unwrap_throw().document().unwrap_throw().create_range().unwrap();
     let mut i = 0;
     let mut f = 0;
 
     let handle_keydown = Closure::wrap(Box::new(move |e: KeyboardEvent| {
-        if track && &e.key() == "Enter" || e.key() == "Tab" {
-            f = get_caret_position();
+        let selection: Node = window().unwrap_throw().get_selection().unwrap().unwrap().focus_node().unwrap();
 
-            range.set_start(_editor.first_child().unwrap().as_ref(), i);
-            range.set_end(_editor.first_child().unwrap().as_ref(), f);
+        if track && &e.key() == "Enter" || e.key() == "Tab" {
+            e.prevent_default();
+            track = false;
+
+            f = get_caret_position();
+            range.set_start(&selection, i);
+            range.set_end(&selection, f);
             range.delete_contents();
             i = 0;
             f = 0;
-        }
-        if &e.key() == "Tab" {
-            track = false;
-            &e.prevent_default();
         }
 
         if &e.key() == "Enter" {
@@ -44,7 +40,7 @@ pub fn insert_components(editor: &Element, trigger: String) {
 
         if track {
             text_value = format!("{}{}", text_value, e.key()).into();
-            log!(&text_value);
+            // log!(&text_value);
         }
 
         if e.key() == trigger {
