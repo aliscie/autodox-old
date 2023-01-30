e14622abd41956e41ef2766fd5ddbc350fb73636
aliscie <alihushamsci@icloud.com>
2023-01-30 13:40:10 +0300

    refactor editor plugins

--- editor/src/app.rs
+++ editor/src/app.rs
@@ -6,12 +6,11 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 use uuid::Uuid;
-use wasm_bindgen::{prelude::Closure, JsCast, UnwrapThrowExt};
-use web_sys::{Element, HtmlInputElement, MutationObserver, MutationObserverInit, MutationRecord, Node, Range, window};
+use wasm_bindgen::{prelude::Closure, JsCast};
+use web_sys::{Element, MutationObserver, MutationObserverInit, MutationRecord, Node, Range, window};
 use yew::prelude::*;
 use yew::{function_component, html};
-use yew::virtual_dom::VNode;
-use crate::plugins::{EditorToolbar, EditorInsert, CommandItems, DropDownItem};
+
 
 /// this captures all the changes in a editor element
 #[derive(Debug)]
@@ -26,6 +25,7 @@ pub struct EditorProps {
     pub title: String,
     pub element_tree: Rc<RefCell<ElementTree>>,
     pub onchange: Callback<EditorChange>,
+    pub children: Children,
 }
 
 // this is used for the work space
@@ -168,43 +168,8 @@ pub fn Editor(props: &EditorProps) -> Html {
     );
 
 
-    let slash_clouser: fn(DropDownItem, Option<Range>) = (|event, range| {
-        let text = event.text.as_str();
-        match text {
-            "table" => {
-                let table = r#"<table><tr> <th>Company</th> <th>Contact</th> <th>Country</th> </tr> <tr> <td>Alfreds Futterkiste</td> <td>Maria Anders</td> <td>Germany</td> </tr> <tr> <td>Centro comercial Moctezuma</td> <td>Francisco Chang</td> <td>Mexico</td> </tr> <tr> <td>Ernst Handel</td> <td>Roland Mendel</td> <td>Austria</td> </tr> <tr> <td>Island Trading</td> <td>Helen Bennett</td> <td>UK</td> </tr> <tr> <td>Laughing Bacchus Winecellars</td> <td>Yoshi Tannamuri</td> <td>Canada</td> </tr> <tr> <td>Magazzini Alimentari Riuniti</td> <td>Giovanni Rovelli</td> <td>Italy</td> </tr></table>"#;
-                let mut table_element = window().unwrap_throw().document().unwrap_throw().create_element("table").unwrap_throw();
-                table_element.set_inner_html(table.replace('\n', "").as_str());
-                let table_element = table_element.first_child().unwrap_throw().unchecked_into::<Element>();
-                let _ = range.unwrap_throw().insert_node(&table_element);
-            }
-            "image" => {}
-            _ => {}
-        };
-    });
-// TODO make the commands Callback<DropDownItem, Option<Range>> instead of fn(DropDownItem, Option<Range>)
-    let emojis_command: fn(DropDownItem, Option<Range>) = (|event, range| {
-// let _ = range.unwrap().insert_node(&window().unwrap_throw().document().unwrap_throw().create_text_node(&event.value));
-        let window = web_sys::window().unwrap();
-        let document = window.document().unwrap();
-        let html_document = document.dyn_into::<web_sys::HtmlDocument>().unwrap();
-        let _ = html_document.exec_command_with_show_ui_and_value("InsertText", false, &event.value).unwrap();
-    });
-
-    let mention_clouser: fn(DropDownItem, Option<Range>) = (|event, range| {
-        log!(event.value);
-    });
-
     let element_tree = props.element_tree.clone();
 
-    let action: Callback<String> = Callback::from(move |e: String| {
-        log!(e.clone());
-// onchange.emit(EditorChange::Update(EditorElementUpdate {
-//     id: element_tree.as_ref().borrow().elements.root.unwrap(),
-//     text_format: Some(format),
-//     ..Default::default()
-// }));
-    });
     let onkeydown: Callback<KeyboardEvent> = Callback::from(move |_e: KeyboardEvent| {
         if _e.key() == "Tab" {
             _e.prevent_default();
@@ -226,12 +191,7 @@ pub fn Editor(props: &EditorProps) -> Html {
             class = "text_editor_container"
             id = "text_editor_container"
            >
-
-            <EditorToolbar  action={action}/>
-            <EditorInsert items={insertion_closures::components()}  trigger={"/".to_string()} command={slash_clouser}/>
-            <EditorInsert items={insertion_closures::mentions()}  trigger={"@".to_string()} command={mention_clouser}/>
-            <EditorInsert items={insertion_closures::emojies()}  trigger={":".to_string()}  command={emojis_command}/>
-
+            {props.children.clone()}
             <div  ref =  {editor_ref}  contenteditable = "true" class="text_editor" id = "text_editor">
             { render(&element_tree.as_ref().borrow(), element_tree.as_ref().borrow().elements.root.unwrap()) }
         </div>
@@ -240,5 +200,3 @@ pub fn Editor(props: &EditorProps) -> Html {
     }
 }
 
-use shared::schema::*;
-use crate::{insertion_closures, plugins};
--- editor/src/plugins/inset_component/types.rs
+++ editor/src/plugins/inset_component/types.rs
@@ -4,10 +4,10 @@ use yew::Html;
 
 #[derive(Clone, PartialEq)]
 pub struct DropDownItem {
-    pub(crate) value: String,
-    pub(crate) text: String,
-    pub(crate) tag: Option<String>,
-    pub(crate) attrs: HashMap<String, String>,
+    pub value: String,
+    pub text: String,
+    pub tag: Option<String>,
+    pub attrs: HashMap<String, String>,
 }
 
 pub type CommandItems = Vec<DropDownItem>;
