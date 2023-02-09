c053296d48e6f3ce2451630da0b5198ac6593b23
lunchspider <lunchspider03@gmail.com>
2023-02-09 20:23:41 +0530

    1. file_node_get call done 2. saves element tree on ic backend

--- canisters/backend/src/lib.rs
+++ canisters/backend/src/lib.rs
@@ -1,18 +1,20 @@
+mod elements;
 mod files;
 mod initialize;
 mod users;
-pub use users::*;
 pub use files::*;
+pub use users::*;
 mod utils;
 
-use initialize::*;
+use crate::files::types::MyStrings;
 use ic_cdk_macros::*;
 use ic_stable_memory::collections::vec::SVec;
-use ic_stable_memory::{s, stable_memory_init, stable_memory_post_upgrade, stable_memory_pre_upgrade};
-use crate::files::types::{MyStrings};
+use ic_stable_memory::{
+    s, stable_memory_init, stable_memory_post_upgrade, stable_memory_pre_upgrade,
+};
+use initialize::*;
 
 pub mod custom_traits;
-
 
 // before upgrading the code of the canister (before deploying the app)
 #[pre_upgrade]
