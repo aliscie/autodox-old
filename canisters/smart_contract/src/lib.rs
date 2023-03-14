9bd76187d813fa7ab32f9cf2a750802ca232c137
Rich Swift <richwon@gmail.com>
2023-03-14 07:12:55 -0700

    Move smart_contract

---
+++ canisters/smart_contract/src/lib.rs
@@ -0,0 +1,27 @@
+
+use ic_cdk::export::candid::{
+    candid_method, check_prog, export_service, CandidType, Deserialize, IDLProg, TypeEnv,
+};
+
+use crate::files::types::MyStrings;
+use ic_cdk_macros::*;
+use ic_stable_memory::collections::vec::SVec;
+use ic_stable_memory::{
+    s, stable_memory_init, stable_memory_post_upgrade, stable_memory_pre_upgrade,
+};
+
+use src::*;
+
+// before upgrading the code of the canister (before deploying the app)
+#[pre_upgrade]
+fn pre_upgrade() {
+    stable_memory_pre_upgrade();
+}
+
+// after upgrading the code of the canister
+#[post_upgrade]
+fn post_upgrade() {
+    stable_memory_post_upgrade(0);
+}
+
+
